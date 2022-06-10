5735fbd854b172b6ee56508a1217972c382142c7
Pedro H. Ribeiro <pedrohr98@hotmail.com>
2022-06-10 16:24:33 -0300

    Aprimorar funções existentes e adicionar novas

--- poc-api/app.py
+++ poc-api/app.py
@@ -8,7 +8,7 @@ def register(id):
  return jsonify({"id": id, "nome": "Pedro", "email": "pedrohr98@hotmail.com"})
  '''
 
-@app.route("/register", methods = ["POST"])
+''' @app.route("/register", methods = ["POST"])
 def register():
     data = request.data
     return data
@@ -16,7 +16,66 @@ def register():
 @app.route("/update-registration", methods = ["PUT"])
 def update():
     data = request.data
-    return data
+    return data '''
+
+# Planos: B - Básico | A - Avançado | I - Ilimitado
+
+# Cadastros
+records = [
+
+    {   
+        "id": 0,
+        "nome": "Pedro",
+        "email": "pedrohr98@hotmail.com",
+        "plano": "A"
+     },
+
+    {
+        "id": 1,
+        "nome": "Anísio",
+        "email": "anisio@hotmail.com",
+        "plano": "I"
+     }
+
+]
+
+# Cadastrar
+@app.route("/register", methods=["POST"])
+def register():
+    register = json.loads(request.data)
+    records.append(register)
+    return jsonify(register, "Cadastrado com sucesso!")
+
+# Listar todos os cadastros
+@app.route("/records", methods=["GET"])
+def get_records():
+    return jsonify("Cadastros: ", records)
+
+# Listar um cadastro específico
+@app.route("/registration/<int:id>", methods=["GET"])
+def get_registration(id):
+    try:
+        registration = records[id]
+        return jsonify("Dados cadastrados: ", registration)
+    except IndexError:
+        return jsonify("Cadastro não encontrado!")
+
+# Atualizar um cadastro
+@app.route("/update-registration/<int:id>", methods=["PUT"])
+def update_registration(id):
+    try:
+        update = json.loads(request.data)
+        records[id] = update
+        return jsonify(update, "Cadastro atualizado com sucesso!")
+    except IndexError:
+        return jsonify("Cadastro não encontrado!")
+
+# Excluir um cadastro
+@app.route("/delete-registration/<int:id>", methods=["DELETE"])
+def delete_registration(id):
+    records.pop(id)
+    return jsonify("Cadastro excluído com sucesso!")
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
